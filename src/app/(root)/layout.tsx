4c4796f11439ce22a2418e87a2818ef9289902a2
Ashok Ginjala <ashokginjala@microsoft.com>
2024-02-20 22:45:33 -0500

    first commit to this public repo

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import MoblieNav from "@/components/shared/MoblieNav";
+import Sidebar from "@/components/shared/Sidebar";
+import { Toaster } from "@/components/ui/toaster";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <Sidebar />
+      <MoblieNav />
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+      <Toaster />
+    </main>
+  );
+};
+
+export default Layout;
---
+++ src/lib/database/mongoose.ts
@@ -0,0 +1,34 @@
+import mongoose, {Mongoose} from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+interface MongooseConnection {
+    conn: Mongoose | null;
+    promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached){
+    cached = (global as any).mongoose = { 
+        conn: null, 
+        promise: null 
+    };
+}
+
+export const connectToDatabase = async () => {
+    if(cached.conn) return cached.conn;
+
+    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+        dbName: 'imaginify',
+        bufferCommands: false
+    });
+
+    cached.conn = await cached.promise;
+
+    console.log('Connected to database');
+
+    return cached.conn;
+}
